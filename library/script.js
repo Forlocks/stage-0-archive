f23177c51ee0158f25f8029b8c12161ba3c24963
forlocks <sheluhen@mail.ru>
2023-08-15 11:30:43 +0300

    fix: make code more readable

--- library/script.js
+++ library/script.js
@@ -1,30 +1,37 @@
 document.querySelector('.burger-icon').addEventListener('click', function(){
   document.querySelector('.header').classList.add('open');
 })
+//Открытие меню при клике по иконке бургер-меню
 
 document.querySelector('.cross-icon').addEventListener('click', function(){
   document.querySelector('.header').classList.remove('open');
 })
+//Закрытие меню при клике по иконке крестика
 
 document.querySelector('.action-1').addEventListener('click', function(){
   document.querySelector('.header').classList.remove('open');
 })
+//Закрытие меню при клике по первой ссылке навигации
 
 document.querySelector('.action-2').addEventListener('click', function(){
   document.querySelector('.header').classList.remove('open');
 })
+//Закрытие меню при клике по второй ссылке навигации
 
 document.querySelector('.action-3').addEventListener('click', function(){
   document.querySelector('.header').classList.remove('open');
 })
+//Закрытие меню при клике по третьей ссылке навигации
 
 document.querySelector('.action-4').addEventListener('click', function(){
   document.querySelector('.header').classList.remove('open');
 })
+//Закрытие меню при клике по четвёртой ссылке навигации
 
 document.querySelector('.action-5').addEventListener('click', function(){
   document.querySelector('.header').classList.remove('open');
 })
+//Закрытие меню при клике по пятой ссылке навигации
 
 document.addEventListener("mouseup", function(event) {
   var obj = document.querySelector(".header-right");
@@ -32,5 +39,6 @@ document.addEventListener("mouseup", function(event) {
     document.querySelector('.header').classList.remove('open');
   }
 });
+//Закрытие меню при клике вне его области
 
 console.log('Все пункты выполнены в соответствии с ТЗ.')
